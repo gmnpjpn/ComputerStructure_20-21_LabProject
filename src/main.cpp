753c9d23bcaf42b754d2af30e0cffcc929c84d4a
Germán <75335349+gmnpjpn@users.noreply.github.com>
2021-04-04 19:10:53 +0200

    + Project Added

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+  // INITIALIZE LED AS AN OUTPUT
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
