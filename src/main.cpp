edd423da2320bca4da7fc92aecded37069a70b4e
Germán <75335349+gmnpjpn@users.noreply.github.com>
2021-04-04 19:27:42 +0200

    + Added Startup LED Signal

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,21 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  //THE CODE THAT I INTRODUCE HERE RUNS ONLY WHEN I POWER ON THE ECOBOT
 
-  // INITIALIZE LED AS AN OUTPUT
+  //INITIALIZE LED AS AN OUTPUT
   pinMode(LED_BUILTIN, OUTPUT);
+
+  //LED ON
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  //WAIT FOR TWO SECONDS
+  delay(2000);
+
+  //LED OFF
+  digitalWrite(LED_BUILTIN, LOW);
+
+  //THIS SIGNAL WITH THE LED WILL INDICATE THAT THE ECOBOT IS READY TO EXECUTE THE COMMANDS THAT WE PROGRAM IT
 }
 
 void loop() {
