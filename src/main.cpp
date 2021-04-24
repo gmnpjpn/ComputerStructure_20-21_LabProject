0cff95080adbc1d38690a9910ebe60311110530e
Germán <75335349+gmnpjpn@users.noreply.github.com>
2021-04-24 15:43:13 +0200

    + Added New Functionalities And Restructure Of Methods
    It's necessary to adjust some things to work properly

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
-//-------------------------------------------> LIBRARIES
+//---------------------------------------> LIBRARIES
 #include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
-//-------------------------------------------> ESTO NO SÉ QUE HACE
+//---------------------------------------> ESTO NO SÉ QUE HACE
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-//-------------------------------------------> DEFINES
+//---------------------------------------> DEFINES
 #define SERVOMIN 320
 #define SERVOSTOP 380
 #define SERVOMAX 440
@@ -20,9 +20,13 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define red_led 12
 #define green_led 9
 
+#define pingPin 5
+
+#define button_pin 11
+
 
 void setup() {
-  //-------------------------------------------> INITIALIZATION OF SERVOS, IR SENSORS...
+  //---------------------------------------> INITIALIZATION OF SERVOS, IR SENSORS...
   pwm.begin();
   pwm.setPWMFreq(60);
 
@@ -33,27 +37,29 @@ void setup() {
   pinMode (red_led, OUTPUT);
   pinMode (green_led, OUTPUT);
 
+  pinMode(button_pin, INPUT);
 
-  //-------------------------------------------> START SIGNAL WITH LED, IT WILL INDICATE THAT THE ECOBOT STARTS RUNNING
+
+  //---------------------------------------> START SIGNAL WITH LED, IT WILL INDICATE THAT THE ECOBOT STARTS RUNNING
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
   delay(500);
   digitalWrite(LED_BUILTIN, LOW);
 }
 
-//-------------------------------------------> METHODS
+//---------------------------------------> METHODS
   void delayMod(){
     delay(0);
   }
 
-  void adelanteRapido() {
+  void fastForward() {
     digitalWrite(green_led,HIGH);
     pwm.setPWM(servo_right, 0, SERVOMAX);
     pwm.setPWM(servo_left, 0, SERVOMIN);
     digitalWrite(green_led,LOW);
   }
 
-  void giroDerecha(){
+  void turnRight(){
     digitalWrite(red_led,HIGH);
     pwm.setPWM(servo_right,0,350);
     pwm.setPWM(servo_left,0,SERVOMIN);
@@ -61,7 +67,7 @@ void setup() {
     digitalWrite(red_led,LOW);
   }
 
-  void giroIzquierda(){
+  void turnLeft(){
     digitalWrite(red_led,HIGH);
     pwm.setPWM(servo_right,0,SERVOMAX);
     pwm.setPWM(servo_left,0,360);
@@ -75,31 +81,137 @@ void setup() {
     pwm.setPWM(servo_left,0,SERVOSTOP);
   }
 
+  void followLine_Right(){
+    //---------------------------------------> LINE FOLLOWING CODE (RIGTH SIDE)
+    int valor_IR_left = digitalRead(IR_left);
+    int valor_IR_right = digitalRead(IR_right);
+
+    if(valor_IR_left==HIGH && valor_IR_right==HIGH){
+      stop();
+    }
+    delayMod();
+    if(valor_IR_left==LOW  && valor_IR_right==LOW){
+      turnRight();
+    }
+    delayMod();
+    if (valor_IR_left==HIGH && valor_IR_right==LOW){
+      turnRight();
+    }
+    delayMod();
+    if (valor_IR_left==LOW && valor_IR_right==HIGH){
+      fastForward();
+    }
+    delayMod();
+  }
+
+  void followLine_Left(){
+    int valor_IR_left = digitalRead(IR_left);
+    int valor_IR_right = digitalRead(IR_right);
+
+    if(valor_IR_left==HIGH && valor_IR_right==HIGH){
+      stop();
+    }
+    delayMod();
+    if(valor_IR_left==LOW  && valor_IR_right==LOW){
+      turnLeft();
+    }
+    delayMod();
+    if (valor_IR_left==HIGH && valor_IR_right==LOW){
+      fastForward();
+    }
+    delayMod();
+    if (valor_IR_left==LOW && valor_IR_right==HIGH){
+      turnLeft();
+    }
+    delayMod();
+  }
+
+  void followLine_Middle(){
+    int valor_IR_left = digitalRead(IR_left);
+    int valor_IR_right = digitalRead(IR_right);
+
+    if(valor_IR_left==HIGH && valor_IR_right==HIGH){
+      stop();
+    }
+    delayMod();
+    if(valor_IR_left==LOW  && valor_IR_right==LOW){
+      fastForward();
+    }
+    delayMod();
+    if (valor_IR_left==HIGH && valor_IR_right==LOW){
+      turnRight();
+    }
+    delayMod();
+    if (valor_IR_left==LOW && valor_IR_right==HIGH){
+      turnLeft();
+    }
+    delayMod();    
+  }
+
+  void turn180Degree(){
+    turnRight();
+  }
+
+  void showMemoryAdressesOfVariables(){
+    int valor_IR_left = digitalRead(IR_left);
+    int *valorIRleft_Pointer= &valor_IR_left;
+    printf("Dirección de memoria de IR izquierdo: %p \n",valorIRleft_Pointer);
+    int valor_IR_right = digitalRead(IR_right);
+    int *valorIRright_Pointer= &valor_IR_right;
+    printf("Dirección de memoria de IR derecho: %p \n",valorIRright_Pointer);
+  }
+
 
 void loop() {
-  //-------------------------------------------> ASSIGNATION OF IR VALUES TO IT'S VARIABLES
+  /*
+  //---------------------------------------> ASSIGNATION OF IR VALUES TO IT'S VARIABLES AND IMPRESSION OF THEIR MEMORY ADRESSES
   int valor_IR_left = digitalRead(IR_left);
+  int *valorIRleft_Pointer= &valor_IR_left;
+  printf("Dirección de memoria de IR izquierdo: %p \n",valorIRleft_Pointer);
   int valor_IR_right = digitalRead(IR_right);
+  int *valorIRright_Pointer= &valor_IR_right;
+  printf("Dirección de memoria de IR derecho: %p \n",valorIRright_Pointer);
+  */
 
-  //-------------------------------------------> LINE FOLLOWING CODE
-  if(valor_IR_left==HIGH && valor_IR_right==HIGH){
-    stop();
+
+  //---------------------------------------> LIGHT SENSOR THINGS
+  int light = analogRead(A0);
+
+  //---------------------------------------> ULTRASOUND SENSOR THINGS
+  long duration, cm;
+  pinMode(pingPin, OUTPUT);
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(10);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pingPin, LOW);
+  pinMode(pingPin, INPUT);
+  duration = pulseIn(pingPin, HIGH);
+  cm = duration / 29 / 2;
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  delay(100);
+
+  //---------------------------------------> BUTTON THINGS
+  int button_value = digitalRead(button_pin);
+  if (button_value==HIGH){
+    pwm.setPWM(servo_right,0,350);
+    pwm.setPWM(servo_left,0,SERVOMIN);
   }
-  delayMod();
-  if(valor_IR_left==LOW  && valor_IR_right==LOW){
-    adelanteRapido();
+
+
+  followLine_Middle();
+
+  if (light>=100){followLine_Left;} else {followLine_Right;}
+  //CAMBIAR EL VALOR 100 POR LO QUE CORRESPONDA, ES PARA QUE NO DE ERROR SI LO DEJO VACÍO
+  
+  if (button_value==HIGH){
+    turn180Degree();
   }
-  delayMod();
-  if (valor_IR_left==HIGH && valor_IR_right==LOW){
-    giroDerecha();
-  }
-  delayMod();
-  if (valor_IR_left==LOW && valor_IR_right==HIGH){
-    giroIzquierda();
-  }
-  delayMod();
+  
 }
 
+//CONFIGURAR LO RELACIONADO CON EL SENSOR DE ULTRASONIDOS PARA QUE ESQUIVE OBSTÁCULOS
 
-//IMPLEMENTAR DE ALGUNA FORMA QUE EL ECOBOT VAYA POR EL BORDE DERECHO Y POR EL BORDE IZQUIERDO TODO EL RECORRIDO.
-//CONFIGURAR LED ROJO PARA LOS CAMBIOS DE SENTIDO Y LED VERDE PARA MARCHA NORMAL.
+//CONFIGURAR EL SENSOR DE LUZ PARA QUE AL DETECTAR EL FLASH, VAYA POR EL BORDE IZQUIERDO Y AL APAGARLO VAYA POR EL BORDE DERECHO
